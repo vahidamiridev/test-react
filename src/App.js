8a816a7e6693839fdd098705a0fbfb59a31debda
vahidamiridev <vahidamiri.dev@gmail.com>
2023-02-22 23:54:20 +0330

    [FEAT]:added my question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [number , setNumber] = useState(0)
+  const [isActive , setIsActive] = useState(false)
+
+  useEffect(()=>{
+      console.log(number)
+      console.log(isActive)
+  },[number])
+
+  const changeNumberHandler =  ()=>{
+      //  setNumber(number + 1)
+      // console.log(number)
+  }
+
+  const changeThemeHandler = ()=>{
+    if(number>= 2) {
+       setIsActive(true)
+      // console.log(isActive)
+    }
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={changeNumberHandler}>ChangeNaumber</button>
+      <button onClick={changeThemeHandler}>ChangeTheme</button>
+
+ 
     </div>
   );
 }
